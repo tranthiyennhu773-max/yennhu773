df2a15f6cdbd435ae7c3ac1483f61fd9b21594a1
tranthiyennhu773-max <tranthiyennhu773@gmail.com>
2025-11-21 08:50:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Trần Thị Yến Như đã load xong!');
+    alert('Chào mừng bạn đến với website portfolio của Yến Như ✨');
+});
